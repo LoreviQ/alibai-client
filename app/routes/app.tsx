081c18ab3c757fd645f046609962c295794a1434
LoreviQ <lorevi@protonmail.com>
2024-11-20 19:32:07 +0000

    Hopefully added app as a protected endpoint

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,3 +1,20 @@
+import { LoaderFunction, redirect } from "@remix-run/node";
+import { authCookie } from "~/utils/cookies";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async ({ request }) => {
+    const cookieHeader = request.headers.get("Cookie");
+    const userData = await authCookie.parse(cookieHeader);
+
+    if (!userData?.authenticated) {
+        return redirect("/login");
+    }
+
+    return { userData };
+};
+
 export default function APP() {
-    return <div>App page</div>;
+    const loaderdata = useLoaderData<typeof loader>();
+    // display user info here
+    return <div>App page: Logged in as {loaderdata.userData.username}</div>;
 }
